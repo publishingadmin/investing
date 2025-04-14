298f38bde010b4c2e071807730df967c1c2b358b
publishingadmin <publishing@gargency.com>
2025-04-14 22:55:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ def estrai_dati():
         return jsonify({"error": "Missing slug"}), 400
 
     url = f"https://it.investing.com/equities/{slug}-ratios"
+    api_key = "38d5cb26de37e1c35bb61c8d50406aaa"
+    proxy_url = f"http://api.scraperapi.com?api_key={api_key}&url={url}"
 
     try:
-        res = requests.get(url, headers=headers)
+        res = requests.get(proxy_url, headers=headers)
         soup = BeautifulSoup(res.text, 'html.parser')
 
         tabella = soup.find('table')
@@ -25,9 +27,9 @@ def estrai_dati():
             return jsonify({"error": "Table not found"}), 404
 
         valori = {
-            "EPS Diluiti ANN": "N/D",
-            "EPS MRQ YoY": "N/D",
-            "Vendite MRQ YoY": "N/D"
+            "Diluted EPS": "N/D",
+            "EPS YoY MRQ": "N/D",
+            "Sales YoY MRQ": "N/D"
         }
 
         for row in tabella.find_all('tr'):
@@ -37,20 +39,14 @@ def estrai_dati():
                 valore = celle[1].text.strip()
 
                 if "Diluted EPS" in nome:
-                    valori["EPS Diluiti ANN"] = valore
-                elif "EPS(MRQ) vs Qtr. 1 Yr. AgoMRQ" in nome:
-                    valori["EPS MRQ YoY"] = valore
-                elif "Sales (MRQ) vs Qtr. 1 Yr. AgoMRQ" in nome:
-                    valori["Vendite MRQ YoY"] = valore
+                    valori["Diluted EPS"] = valore
+                elif "EPS (MRQ) vs" in nome:
+                    valori["EPS YoY MRQ"] = valore
+                elif "Sales (MRQ) vs" in nome:
+                    valori["Sales YoY MRQ"] = valore
 
         return jsonify(valori)
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
 
